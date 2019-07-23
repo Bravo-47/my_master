31c1816d6a355df517d420016742c07a6251a612
Ramon <rhv.rmn.vnvch@gmail.com>
2019-07-23 20:02:46 +0600

    asdfasdf

---
+++ index.php
@@ -0,0 +1,6 @@
+
+ <form class="my-form" action="index.php" method="post">
+   <input type="text" name="name" value="">
+   <textarea name="description" rows="8" cols="80"></textarea>
+   <input type="date" name="date" value="">
+ </form>
