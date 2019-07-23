ef628461b4afc3dd403bebbe671cc7d461b42590
Ramon <rhv.rmn.vnvch@gmail.com>
2019-07-23 20:07:21 +0600

    FIrst file;

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-
+<?php
+echo "Hello";
+?>
  <form class="my-form" action="index.php" method="post">
    <input type="text" name="name" value="">
    <textarea name="description" rows="8" cols="80"></textarea>
