5ce149bfcfd5b29fddeec5785bd3b8b14e02b950
Roman Orehov <orehov_roman@mail.ru>
2019-07-25 15:44:58 +0600

    Change structure

--- finishView.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-primary" role="alert">
-  Данные могут быть успешно сохранены в таблице.
-  <a href="index.php" class="btn btn-primary">Сначала</a>
-</div>
--- form3View.php
+++
@@ -1,3 +0,0 @@
-<form class="my-form3" action="index.php" method="post">
-  <input type="submit" name="save" value="Подтвердить">
-</form>
